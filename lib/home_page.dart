c8bb86fe3128227c12e503963f10ecee133ea6ac
tania cubillos <tjyaimacubillos@gmail.com>
2026-07-03 08:49:20 -0500

    version 1

---
+++ lib/home_page.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Hoja de Vida"),
+      ),
+
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(20),
+
+        child: Column(
+          children: [
+
+            CircleAvatar(
+              radius: 60,
+              backgroundImage:
+              AssetImage("assets/imagenes/foto.jpg"),
+            ),
+
+            SizedBox(height: 20),
+
+            Text(
+              "JUAN PEREZ",
+              style: TextStyle(
+                fontSize: 25,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+
+            SizedBox(height: 20),
+
+            TextField(
+              decoration: InputDecoration(
+                labelText: "Nombre",
+                border: OutlineInputBorder(),
+                prefixIcon: Icon(Icons.person),
+              ),
+            ),
+
+            SizedBox(height: 15),
+
+            TextField(
+              decoration: InputDecoration(
+                labelText: "Correo",
+                border: OutlineInputBorder(),
+                prefixIcon: Icon(Icons.email),
+              ),
+            ),
+
+            SizedBox(height: 15),
+
+            TextField(
+              decoration: InputDecoration(
+                labelText: "Teléfono",
+                border: OutlineInputBorder(),
+                prefixIcon: Icon(Icons.phone),
+              ),
+            ),
+
+            SizedBox(height: 20),
+
+            ElevatedButton(
+              onPressed: () {
+                ScaffoldMessenger.of(context)
+                    .showSnackBar(
+                  const SnackBar(
+                    content:
+                    Text("Información guardada"),
+                  ),
+                );
+              },
+              child: const Text("Guardar"),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
